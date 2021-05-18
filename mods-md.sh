c7df7d0645ffecb9bfde4f2850660eb2ec0912d8
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-05-18 13:54:28 +0200

    Filter for plugins and mods to only have .py and .jar

--- mods-md.sh
+++ mods-md.sh
@@ -3,11 +3,11 @@
 rm /home/server/scripts/files/mods.md
 touch /home/server/scripts/files/mods.md
 printf '# Survival mods\n```\n' >> /home/server/scripts/files/mods.md
-ls /home/server/survival/server/mods >> /home/server/scripts/files/mods.md
+ls /home/server/survival/server/mods | grep *.jar >> /home/server/scripts/files/mods.md
 printf '```\n# Creative mods\n```\n' >> /home/server/scripts/files/mods.md
 ls /home/server/creative/server/mods >> /home/server/scripts/files/mods.md
 printf '```\n# MCDR Plugins\n```\n' >> /home/server/scripts/files/mods.md
-ls /home/server/survival/plugins >> /home/server/scripts/files/mods.md
+ls /home/server/survival/plugins | grep *.py >> /home/server/scripts/files/mods.md
 printf '```\n# carpet.conf\n```\n' >> /home/server/scripts/files/mods.md
 cat /home/server/survival/server/world/carpet.conf >> /home/server/scripts/files/mods.md
 echo '```'  >> /home/server/scripts/files/mods.md
