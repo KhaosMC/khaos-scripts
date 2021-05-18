c461e1d8eacae5969edb90127e12e371150d3d1f
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-05-18 15:14:08 +0200

    Update mods-md.sh

--- mods-md.sh
+++ mods-md.sh
@@ -11,4 +11,4 @@ cd /home/server/survival/plugins/ && printf '%s\n' *.py >> /home/server/scripts/
 printf '```\n# carpet.conf\n```properties\n' >> /home/server/scripts/files/mods.md
 cat /home/server/survival/server/world/carpet.conf >> /home/server/scripts/files/mods.md
 echo '```'  >> /home/server/scripts/files/mods.md
-gist-paste -u 7f80ab2b67a780f52a46bfa2e2808c95 /home/server/scripts/files/mods.md 
+cd /home/server/scripts/7f80ab2b67a780f52a46bfa2e2808c95 && mv /home/server/scripts/files/mods.md ../7f80ab2b67a780f52a46bfa2e2808c95 && git commit -m 'Daily mods.md update' && git push
