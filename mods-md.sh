e110b23f70bfda80f135cf3f7705f2eebbd95ab0
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-05-18 15:05:26 +0200

    Fix formatting for carpet.conf

--- mods-md.sh
+++ mods-md.sh
@@ -8,7 +8,7 @@ printf '```\n# Creative mods\n```java\n' >> /home/server/scripts/files/mods.md
 printf '%s\n' /home/server/creative/server/mods/*.jar >> /home/server/scripts/files/mods.md
 printf '```\n# MCDR Plugins\n```py\n' >> /home/server/scripts/files/mods.md
 printf '%s\n' /home/server/survival/plugins/*.py >> /home/server/scripts/files/mods.md
-printf '```\n# carpet.conf\n```\n' >> /home/server/scripts/files/mods.md
+printf '```\n# carpet.conf\n```properties\n' >> /home/server/scripts/files/mods.md
 cat /home/server/survival/server/world/carpet.conf >> /home/server/scripts/files/mods.md
 echo '```'  >> /home/server/scripts/files/mods.md
 gist-paste -u 7f80ab2b67a780f52a46bfa2e2808c95 /home/server/scripts/files/mods.md 
