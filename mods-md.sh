2092725fc2f85f04f7f6ae44e31f41a85608f6ca
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-05-18 13:49:32 +0200

    Improve formatting for the .md file

--- mods-md.sh
+++ mods-md.sh
@@ -2,12 +2,12 @@
 #!/bin/bash
 rm /home/server/scripts/files/mods.md
 touch /home/server/scripts/files/mods.md
-printf '# Survival mods\n```' >> /home/server/scripts/files/mods.md
+printf '# Survival mods\n```\n' >> /home/server/scripts/files/mods.md
 ls /home/server/survival/server/mods >> /home/server/scripts/files/mods.md
-printf '```\n# Creative mods\n```' >> /home/server/scripts/files/mods.md
+printf '```\n# Creative mods\n```\n' >> /home/server/scripts/files/mods.md
 ls /home/server/creative/server/mods >> /home/server/scripts/files/mods.md
-printf '```\n# MCDR Plugins\n```' >> /home/server/scripts/files/mods.md
+printf '```\n# MCDR Plugins\n```\n' >> /home/server/scripts/files/mods.md
 ls /home/server/survival/plugins >> /home/server/scripts/files/mods.md
-printf '```\n# carpet.conf\n```' >> /home/server/scripts/files/mods.md
+printf '```\n# carpet.conf\n```\n' >> /home/server/scripts/files/mods.md
 cat /home/server/survival/server/world/carpet.conf >> /home/server/scripts/files/mods.md
 echo '```'  >> /home/server/scripts/files/mods.md
