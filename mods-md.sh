43674a1f2a487ff31ae6cd2d206e5c57e9dad44b
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-05-18 15:02:07 +0200

    Fix formatting again

--- mods-md.sh
+++ mods-md.sh
@@ -3,11 +3,11 @@
 rm /home/server/scripts/files/mods.md
 touch /home/server/scripts/files/mods.md
 printf '# Survival mods\n```java\n' >> /home/server/scripts/files/mods.md
-ls /home/server/survival/server/mods | grep .jar >> /home/server/scripts/files/mods.md
+printf '%s\n' /home/server/survival/server/mods/*.jar >> /home/server/scripts/files/mods.md
 printf '```\n# Creative mods\n```java\n' >> /home/server/scripts/files/mods.md
-ls /home/server/creative/server/mods | grep .jar >> /home/server/scripts/files/mods.md
+printf '%s\n' /home/server/creative/server/mods/*.jar >> /home/server/scripts/files/mods.md
 printf '```\n# MCDR Plugins\n```py\n' >> /home/server/scripts/files/mods.md
-ls /home/server/survival/plugins | grep .py >> /home/server/scripts/files/mods.md
+printf '%s\n' /home/server/survival/plugins/*.py >> /home/server/scripts/files/mods.md
 printf '```\n# carpet.conf\n```\n' >> /home/server/scripts/files/mods.md
 cat /home/server/survival/server/world/carpet.conf >> /home/server/scripts/files/mods.md
 echo '```'  >> /home/server/scripts/files/mods.md
