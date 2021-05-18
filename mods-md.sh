b139c1670656cd1823c1aab2698cc972b91989fc
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-05-18 13:56:30 +0200

    Better formating for MD files

--- mods-md.sh
+++ mods-md.sh
@@ -2,11 +2,11 @@
 #!/bin/bash
 rm /home/server/scripts/files/mods.md
 touch /home/server/scripts/files/mods.md
-printf '# Survival mods\n```\n' >> /home/server/scripts/files/mods.md
+printf '# Survival mods\n```java\n' >> /home/server/scripts/files/mods.md
 ls /home/server/survival/server/mods | grep *.jar >> /home/server/scripts/files/mods.md
-printf '```\n# Creative mods\n```\n' >> /home/server/scripts/files/mods.md
+printf '```\n# Creative mods\n```java\n' >> /home/server/scripts/files/mods.md
 ls /home/server/creative/server/mods >> /home/server/scripts/files/mods.md
-printf '```\n# MCDR Plugins\n```\n' >> /home/server/scripts/files/mods.md
+printf '```\n# MCDR Plugins\n```py\n' >> /home/server/scripts/files/mods.md
 ls /home/server/survival/plugins | grep *.py >> /home/server/scripts/files/mods.md
 printf '```\n# carpet.conf\n```\n' >> /home/server/scripts/files/mods.md
 cat /home/server/survival/server/world/carpet.conf >> /home/server/scripts/files/mods.md
