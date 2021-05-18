461cc42de24c588e06fdf1215b22adc56c65362a
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-05-18 13:45:26 +0200

    fix all absolute paths..

--- mods-md.sh
+++ mods-md.sh
@@ -2,12 +2,12 @@
 #!/bin/bash
 rm /home/server/scripts/files/mods.md
 touch /home/server/scripts/files/mods.md
-printf '# Survival mods\n```' >> mods.md
-ls /home/server/survival/server/mods >> mods.md
-printf '```\n# Creative mods\n```' >> mods.md
-ls /home/server/creative/server/mods >> mods.md
-printf '```\n# MCDR Plugins\n```' >> mods.md
-ls /home/server/survival/plugins >> mods.md
-printf '```\n# carpet.conf\n```' >> mods.md
-cat /home/server/survival/server/world/carpet.conf >> mods.md
-echo '```'  >> mods.md
+printf '# Survival mods\n```' >> /home/server/scripts/files/mods.md
+ls /home/server/survival/server/mods >> /home/server/scripts/files/mods.md
+printf '```\n# Creative mods\n```' >> /home/server/scripts/files/mods.md
+ls /home/server/creative/server/mods >> /home/server/scripts/files/mods.md
+printf '```\n# MCDR Plugins\n```' >> /home/server/scripts/files/mods.md
+ls /home/server/survival/plugins >> /home/server/scripts/files/mods.md
+printf '```\n# carpet.conf\n```' >> /home/server/scripts/files/mods.md
+cat /home/server/survival/server/world/carpet.conf >> /home/server/scripts/files/mods.md
+echo '```'  >> /home/server/scripts/files/mods.md
