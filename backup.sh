4b31893c3dbb18ca1b38a5f98dde0f83a4ea1347
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-05-31 10:13:04 +0200

    fixes

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 # Simple backup script by vktec
 # Backup script to backup Survival and Creative server, needs lzip
-#!/bin/bash
 # Survival
-cd /home/server/survival && tar --lzip -cf "backups/$(date -u +%Y-%M-%dT%H:%m:%S).tar.lz" server/world/
+cd /home/server/survival && tar --lzip -cf "backups/$(date -u +%Y-%m-%dT%H:%M:%S).tar.lz" server/world/
 # Creative
-cd /home/server/creative && tar --lzip -cf "backups/$(date -u +%Y-%M-%dT%H:%m:%S).tar.lz" server/world/
+cd /home/server/creative && tar --lzip -cf "backups/$(date -u +%Y-%m-%dT%H:%M:%S).tar.lz" server/world/
